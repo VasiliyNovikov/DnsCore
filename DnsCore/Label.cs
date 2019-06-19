fceaae0c4fd9fff43cef4b1f57421359b326d2a8
Vasiliy Novikov <vanoviko@microsoft.com>
2019-06-19 03:27:07 +0300

    Work in progress - labels & names

---
+++ DnsCore/Label.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DnsCore
+{
+    public class Label
+    {
+        private const int MaxLength = 63;
+
+        private readonly string _label;
+
+        public Label(string label)
+        {
+            if (label == null)
+                throw new ArgumentNullException(nameof(label));
+            if (label.Length > MaxLength)
+                throw new ArgumentException($"Label should have length less then or equal to {MaxLength}", nameof(label));
+
+            _label = label ?? String.Empty;
+        }
+
+        public override string ToString() => _label;
+
+        public static implicit operator Label(string label) => new Label(label);
+        public static implicit operator string(Label label) => label._label;
+    }
+}
---
+++ DnsCore/Name.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DnsCore
+{
+    public class Name
+    {
+        public IReadOnlyList<Label> Labels { get; }
+
+        public Name(IReadOnlyCollection<Label> labels)
+        {
+            if (labels == null)
+                throw new ArgumentNullException(nameof(labels));
+            if (labels.Count == 0)
+                throw new ArgumentException("Number of labels must be greater than 0", nameof(labels));
+
+            Labels = labels.ToList();
+        }
+
+        public Name(string name)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+            var labelStrings = name.Split('.', StringSplitOptions.RemoveEmptyEntries);
+            var labels = new Label[labelStrings.Length];
+            for (var i = 0; i < labels.Length; ++i)
+                labels[i] = labelStrings[i];
+            Labels = labels;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            foreach (var label in Labels)
+            {
+                builder.Append(label);
+                builder.Append(".");
+            }
+            return builder.ToString();
+        }
+
+        public static implicit operator Name(string name) => new Name(name);
+        public static implicit operator string(Name name) => name.ToString();
+    }
+}
