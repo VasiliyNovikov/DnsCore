abec9373429f39838fae6d5e39bea0b3e2b800d4
Vasyl Novikov <vasiliy.novikov@outlook.com>
2026-01-22 14:12:42 +0200

    Preparatory refactorings for DNS client: Request ID Generator - use RandomNumberGenerator, add missing ConfigureAwait

---
+++ DnsCore/Model/DnsRequestIdGenerator.cs
@@ -0,0 +1,8 @@
+﻿using System.Security.Cryptography;
+
+namespace DnsCore.Model;
+
+internal static class DnsRequestIdGenerator
+{
+    public static ushort NextId() => (ushort)RandomNumberGenerator.GetInt32(ushort.MaxValue);
+}
